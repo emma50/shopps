0c8493cf6bd23b2fef0c3d592b95b172f0cab769
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2023-01-15 23:42:42 +0100

    Code refractor

--- utils/db.js
+++ utils/db.js
@@ -18,7 +18,7 @@ async function connectDB () {
     await mongoose.disconnect()
   }
 
-  const db = await mongoose.connect(process.env.MONGODB_URL)
+  const db = await mongoose.connect(process.env.MONGODB_URI)
   console.log('A new connection to the database was made')
   connection.isConnected = db.connections[0].readyState
 }
