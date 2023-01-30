20ae4ceca21908db03c95be3492b0a2feb8aa065
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2023-01-30 09:22:01 +0100

    FE: Implement category cards component - Setup category cards component in the home page - Make category cards responsive

---
+++ components/home/category/index.js
@@ -0,0 +1,30 @@
+import { BsArrowRightCircle } from 'react-icons/bs'
+import { useMediaQuery } from 'react-responsive'
+import styles from './category.module.scss'
+
+export default function Category({ header, products, background }) {
+  const isMedium = useMediaQuery({ query: '(max-width: 1200px)' })
+  const isMobile = useMediaQuery({ query: '(max-width: 550px)' })
+  return (
+    <div className={styles.category} style={{background}}>
+      <div className={styles.category__header}>
+        <h1>{header}</h1>
+        <BsArrowRightCircle/>
+      </div>
+      <div className={styles.category__products}>
+        {
+          products.slice(0, isMobile ? 6 : isMedium ? 4 : 6 ).map((product, index) => {
+            return (
+              <div className={styles.product} key={index}>
+                <img
+                src={product.image}
+                alt="product image"
+              />
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,81 @@
 import { Inter } from '@next/font/google'
 import { useSession, signIn, signOut } from "next-auth/react"
 import axios from 'axios'
+import { useMediaQuery } from 'react-responsive'
 import Header from '../components/header'
 import Footer from '../components/footer'
 import Main from '../components/home/main'
 import styles from '../styles/Home.module.scss'
 import FlashDeals from '../components/home/flashDeals'
+import Category from '../components/home/category'
+import {
+  women_dresses,
+  women_shoes,
+  women_accessories 
+} from '../data/home'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home({ country }) {
   const { data: session } = useSession()
+  const isMedium = useMediaQuery({ query: '(max-width: 850px)' })
+  const isMobile = useMediaQuery({ query: '(max-width: 550px)' })
   return <>
     <Header country={country} />
     <div className={styles.home}>
       <div className={styles.container}>
         <Main/>
         <FlashDeals/>
+        <div className={styles.home__fashion}>
+          <h1>Womens Fashion</h1>
+          <div className={styles.home__category}>
+            <Category
+              header={'Dresses'}
+              products={women_dresses}
+              background={'#5a31f4'}
+            />
+            {
+              !isMedium && (
+                <Category
+                  header={'Shoes'} 
+                  products={women_shoes}
+                  background={'#6cc070'}
+                />
+              )
+            }
+            {
+             isMobile && (
+              <Category
+                header={'Shoes'} 
+                products={women_shoes}
+                background={'#6cc070'}
+              />
+            ) 
+            }
+            <Category
+              header={'Accessaries'} 
+              products={women_accessories}
+              background={'#000'}
+            />
+        </div>
+        </div>
+        {/* <div className={styles.home__category}>
+          <Category
+            header={'Dresses'}
+            products={women_dresses}
+            background={'#5a31f4'}
+          />
+          <Category
+            header={'Shoes / High Heels'} 
+            products={women_shoes}
+            background={'#6cc070'}
+          />
+          <Category
+            header={'Accessaries'} 
+            products={women_accessories}
+            background={'#000'}
+          />
+        </div> */}
       </div>
     </div>
     <Footer country={country} />
