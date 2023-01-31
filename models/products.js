f0da3475c12b6041422b3e70272a998a5c75fde6
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2023-01-31 05:16:22 +0100

    BE: Create product model

---
+++ models/products.js
@@ -0,0 +1,128 @@
+import mongoose from 'mongoose';
+
+const { ObjectId } = mongoose.Schema
+
+const reviewSchema = new mongoose.Schema({
+  reviewBy: {
+    type: ObjectId,
+    ref: 'User',
+    required: true
+  },
+  rating: {
+    type: Number,
+    default: 0,
+    required: true
+  },
+  review: {
+    type: String,
+    required: true
+  },
+  style: {
+    color: String,
+    image: String
+  },
+  fit: {
+    type: String
+  },
+  images: [],
+  like: []
+})
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please enter the product name']
+  },
+  description: { 
+    type: String,
+    required: [true, 'Please enter a description for this product'],
+    trim: true,
+    unique: true  
+  },
+  brand: {
+    type: String
+  },
+  slug: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: ObjectId,
+    required: true,
+    ref: 'Category'
+  },
+  subCategories: [
+    {
+      type: ObjectId
+    }
+  ],
+  details: [
+    {
+      name: String,
+      value: String
+    }
+  ],
+  questions: [
+    {
+      question: String,
+      answer: String
+    }
+  ],
+  reviews: [reviewSchema],
+  refundPolicy: {
+    type: String,
+    default: '30 days'
+  },
+  /* user: {
+    type: String,
+    required: true
+  }, */
+  rating: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  numReviews: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  shipping: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  subProducts: [
+    {
+      images: [],
+      description_images: [],
+      color:{
+        color: {
+          type: String
+        },
+        image: {
+          type: String
+        }
+      },
+      sizes: [
+        {
+          size: String,
+          quantity: Number,
+          price: Number
+        },
+      ],
+      discount: {
+        type: Number,
+        default: 0
+      },
+      sold: {
+        type: Number,
+        default: 0
+      }
+    }
+  ]
+}, { timestamps: true });
+
+const Product = mongoose.models.Product || mongoose.model('Product', productSchema);
+
+export default Product;
